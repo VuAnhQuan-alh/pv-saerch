3ac418c0c7674eb62179951150766f15473ec786
Vu Anh Quan <quanva2521@gmail.com>
2022-06-25 19:59:35 +0700

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
-import { getBooks } from "./api";
+// import { useEffect } from "react";
+// import { getBooks } from "./api";
 
 function App() {
-  const getData = async () => {
-    const data = await getBooks();
-    console.log(data);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-  return <div className="App">search component</div>;
+  // const getData = async () => {
+  //   const data = await getBooks();
+  //   console.log(data);
+  // };
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  return (
+    <div className="container mx-auto py-4 text-slate-800 font-sans">
+      Search component
+    </div>
+  );
 }
 
 export default App;
