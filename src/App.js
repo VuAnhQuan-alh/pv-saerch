a6b13d29a980dd30555585482dfc07d5d29140bf
Vu Anh Quan <quanva2521@gmail.com>
2022-06-26 15:22:29 +0700

    complete alg and fake 20k data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-// import { useEffect } from "react";
-// import { getBooks } from "./api";
+import { useRef, useState } from "react";
+import listBook from "./dataFake";
 
 function App() {
-  // const getData = async () => {
-  //   const data = await getBooks();
-  //   console.log(data);
-  // };
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  const inRef = useRef(null);
+  const [items, setItems] = useState([]);
+  const onKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      const result = listBook
+        .filter((value) => {
+          const { title } = value;
+          return inRef.current.value
+            .split(" ")
+            .every(
+              (item) =>
+                title.toLowerCase().includes(item) &&
+                title.split(" ").some((el) => el.toLowerCase().startsWith(item))
+            );
+        })
+        .filter((el, idx) => idx < 19);
+      setItems(result);
+    }
+  };
 
   return (
     <div className="container mx-auto py-4 text-slate-800 font-sans">
       Search component
+      <div className="mt-3 flex">
+        <input
+          onKeyDown={onKeyPressed}
+          ref={inRef}
+          type="text"
+          className="w-72 border border-slate-400 py-1 px-3 placeholder:text-slate-400 focus:outline-noe rounded"
+          placeholder="Enter search"
+        />
+      </div>
+      <div className="mt-5">
+        <ul className="list-disc">
+          {items.length > 0
+            ? items.map((el, idx) => <li key={idx}>{el?.title}</li>)
+            : "search books..."}
+        </ul>
+      </div>
     </div>
   );
 }
