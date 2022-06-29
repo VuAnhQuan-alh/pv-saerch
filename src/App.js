64745fcd7bf85ddcfe82847a41a58395bff97747
Vu Anh Quan <quanva2521@gmail.com>
2022-06-29 22:10:00 +0700

    update case match length and chart

--- src/App.js
+++ src/App.js
@@ -28,6 +28,19 @@ function App() {
       <div className="mt-3 flex">
         <input
           onKeyDown={onKeyPressed}
+          onChange={(event) => {
+            let words = event.target.value;
+            const isMatchLength = words
+              .split(" ")
+              .every(
+                (el) =>
+                  el.length < 15 &&
+                  new RegExp(/^[A-Za-z0-9 ]*$/g).test(el.charAt(el.length - 1))
+              );
+            if (!isMatchLength) {
+              inRef.current.value = words.slice(0, words.length - 1);
+            }
+          }}
           ref={inRef}
           type="text"
           className="w-72 border border-slate-400 py-1 px-3 placeholder:text-slate-400 focus:outline-noe rounded"
